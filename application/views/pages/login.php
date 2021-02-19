db7563a36fa8b01a31da6b5a72b19417587091ab
DESKTOP-D71JV4G\HP <ntaleian@gmail.com>
2021-02-19 04:31:05 +0300

    favicon changes
    some changes to the favicons

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="http://bx-code-admin.websitedesignmarketingagency.com/images/favicon.ico">
+    <link rel="icon" href="<?php echo base_url(); ?>assets/images/favicon.ico">
 
     <title>PCA SCOC - Log in </title>
   
